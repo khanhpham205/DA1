1c951625742a7fcf115d65ed450f65f19bf0909c
khanhpham205 <khanhhuu123456@gmail.com>
2024-11-09 09:15:27 +0700

    Revert "a"
    This reverts commit 9b31f8ec3986640f9e5e1d0dfe33108e9921562d.

--- index.php
+++ index.php
@@ -1,19 +1,4 @@
 <?php
 ob_start();
-include_once('models/sanpham.php');
-include_once('models/danhmuc.php');
-include_once('models/hang.php');
-include_once('controllers/pageController.php');
-
-$page = (isset($_GET['page'])) ? $_GET['page']: null;
-$pageC = new PageController;
-switch($page){
-    case 'home':
-        
-        $pageC->home();
-        break;
-    default:
-        $pageC->home();
-}
-
+header('Location: controllers');
 ?>
